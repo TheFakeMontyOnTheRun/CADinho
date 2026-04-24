1f20147c00a15d8986fc8827abe5b5b35844c15c
Daniel Monteiro <monteiroquiet@gmail.com>
2026-04-24 23:06:07 +0100

    Some very simple grid compose

--- composeApp/src/commonMain/kotlin/pt/b13h/cadinho/App.kt
+++ composeApp/src/commonMain/kotlin/pt/b13h/cadinho/App.kt
@@ -1,6 +1,7 @@
 package pt.b13h.cadinho
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -13,37 +14,114 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 
 import cadinho.composeapp.generated.resources.Res
 import cadinho.composeapp.generated.resources.compose_multiplatform
 
 @Composable
+fun Grid() {
+
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+
+
+        /*
+
+          /\
+          \/\
+           \/
+
+        * */
+
+
+
+        for (cy in 0..31) {
+            for (cx in 0..31) {
+
+                // *\
+                // **
+                drawLine(
+                    color = Color.Blue,
+                    start = Offset(
+                        x = (-(cy + 1) * 10 + (cx * 10) + canvasWidth / 2).toFloat(),
+                        y = ((cx + 1) * 10 + cy * 10 + canvasHeight / 4).toFloat()
+                    ),
+                    end = Offset(
+                        x = (-(cy + 1) * 10 + (cx + 1) * 10 + canvasWidth / 2).toFloat(),
+                        y = ((cx + 1) * 10 + (cy + 1) * 10 + canvasHeight / 4).toFloat()
+                    ),
+                    strokeWidth = 1.dp.toPx(), // Convert DP to Pixels
+                    cap = StrokeCap.Round
+                )
+
+                // **
+                // */
+                drawLine(
+                    color = Color.Blue,
+                    start = Offset(
+                        x = (-(cy + 1) * 10 + (cx + 1) * 10 + canvasWidth / 2).toFloat(),
+                        y = ((cx + 1) * 10 + (cy + 1) * 10 + canvasHeight / 4).toFloat()
+                    ),
+                    end = Offset(
+                        x = (-(cy + 1) * 10 + (cx) * 10 + canvasWidth / 2).toFloat(),
+                        y = ((cx + 1) * 10 + (cy + 2) * 10 + canvasHeight / 4).toFloat()
+                    ),
+                    strokeWidth = 1.dp.toPx(), // Convert DP to Pixels
+                    cap = StrokeCap.Round
+                )
+
+                //   /*
+                //   **
+                drawLine(
+                    color = Color.Blue,
+                    start = Offset(
+                        x = (-(cy + 1) * 10 + (cx * 10) + canvasWidth / 2).toFloat(),
+                        y = ((cx + 1) * 10 + cy * 10 + canvasHeight / 4).toFloat()
+                    ),
+                    end = Offset(
+                        x = (-(cy + 1) * 10 + (cx - 1) * 10 + canvasWidth / 2).toFloat(),
+                        y = ((cx + 1) * 10 + (cy + 1) * 10 + canvasHeight / 4).toFloat()
+                    ),
+                    strokeWidth = 1.dp.toPx(), // Convert DP to Pixels
+                    cap = StrokeCap.Round
+                )
+
+
+                //  **
+                //  \*
+                drawLine(
+                    color = Color.Blue,
+                    start = Offset(
+                        x = (-(cy + 1) * 10 + (cx - 1) * 10 + canvasWidth / 2).toFloat(),
+                        y = ((cx + 1) * 10 + (cy + 1) * 10 + canvasHeight / 4).toFloat()
+                    ),
+                    end = Offset(
+                        x = (-(cy + 1) * 10 + (cx) * 10 + canvasWidth / 2).toFloat(),
+                        y = ((cx + 1) * 10 + (cy + 2) * 10 + canvasHeight / 4).toFloat()
+                    ),
+                    strokeWidth = 1.dp.toPx(), // Convert DP to Pixels
+                    cap = StrokeCap.Round
+                )
+
+
+            }
+        }
+    }
+
+
+}
+
+@Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+        Grid()
     }
 }
