4b6d491372f9a31789b12c8ea1bfd1450fb60575
Daniel Monteiro <monteiroquiet@gmail.com>
2026-04-27 19:29:22 +0100

    Fix placements in Android

--- composeApp/src/commonMain/kotlin/pt/b13h/cadinho/App.kt
+++ composeApp/src/commonMain/kotlin/pt/b13h/cadinho/App.kt
@@ -3,27 +3,9 @@ package pt.b13h.cadinho
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -335,11 +317,15 @@ fun ComboRow(
 @Preview
 fun App() {
     MaterialTheme {
-        Row(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .windowInsetsPadding(WindowInsets.safeDrawing)
+        ) {
 
             Box(
                 modifier = Modifier
-                    .weight(1f)   // grid takes remaining space
+                    .weight(1f)
                     .fillMaxHeight()
             ) {
                 Grid()
@@ -349,8 +335,7 @@ fun App() {
                 modifier = Modifier
                     .width(320.dp)
                     .fillMaxHeight(),
-                tonalElevation = 6.dp,
-                shadowElevation = 8.dp
+                tonalElevation = 6.dp
             ) {
                 ComboBoxForm()
             }
